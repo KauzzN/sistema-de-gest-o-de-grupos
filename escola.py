42b5a3639d26ea4e63ac7e83f5103f9ed72f942b
KauzzN <167117607+KauzzN@users.noreply.github.com>
2025-09-04 14:31:34 -0300

    iniciando as classes escola e professores

---
+++ escola.py
@@ -0,0 +1,9 @@
+
+
+class Escola:
+    def __init__(self, nome, cidade, bairro):
+        self.nome = nome
+        self.cidade = cidade
+        self.bairro = bairro
+        self.id = None
+        self.turmas = []
