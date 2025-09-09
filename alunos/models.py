4b10f98b5178d121ce348156be3006936adef9ef
Gato Fantasma <Luized3859@gmail.com>
2025-09-08 21:02:03 -0300

    model
    model dos alunos em progresso

--- alunos/models.py
+++ alunos/models.py
@@ -1,5 +1,27 @@
+# alunos/models.py
 from django.db import models
 
-# Create your models here.
 class Turma(models.Model):
     nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+
+class Aluno(models.Model):
+    nome = models.CharField(max_length=100)
+    turma = models.ForeignKey(
+        Turma,
+        on_delete=models.SET_NULL,  # se a turma for deletada, aluno não some
+        null=True,
+        blank=True,
+        related_name="alunos"
+    )
+    status = models.CharField(
+        max_length=20,
+        choices=[("ativo", "Ativo"), ("inativo", "Inativo"), ("nenhum", "Nenhum")],
+        default="nenhum"
+    )
+
+    def __str__(self):
+        return self.nome
